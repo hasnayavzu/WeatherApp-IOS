202bb8d50d53532e03878386f5def758691682ae
hasnayavzu <hasan.yavuz@ozu.edu.tr>
2023-03-06 02:37:34 +0300

    created necessary views and components in main view.

---
+++ weather-app/WeatherViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  weather-app
+//
+//  Created by Hasan Yavuz on 6.03.2023.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var conditionLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func addLocationButtonTapped(_ sender: Any) {
+    }
+    
+    @IBAction func locationButtonTapped(_ sender: Any) {
+    }
+    
+}
+
