0d8c3413b05ca8cf194a7fb28de6ed22e9d7eda4
hasnayavzu <hasan.yavuz@ozu.edu.tr>
2023-04-08 01:40:30 +0300

    added smth

--- weather-app/WeatherViewController.swift
+++ weather-app/WeatherViewController.swift
@@ -29,20 +29,33 @@ class WeatherViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        showAnimation()
-        fetchWeather()
+        fetchWeather(byCity: "Istanbul")
     }
     
-    private func fetchWeather() {
-        
-        weatherManager.fetchWeather(byCity: "Istanbul") { [weak self] (result) in
+    private func fetchWeather(byLocation location: CLLocation) {
+        showAnimation()
+        let lat = location.coordinate.latitude
+        let lon = location.coordinate.longitude
+        weatherManager.fetchWeather(lat: lat, lon: lon) { [weak self] (result) in
+            guard let this = self else { return }
+            this.handleResult(result)
+        }
+    }
+    
+    private func fetchWeather(byCity city: String) {
+        showAnimation()
+        weatherManager.fetchWeather(byCity: city) { [weak self] (result) in
             guard let this = self else {return}
-            switch result {
-            case .success(let model):
-                this.updateView(with: model)
-            case .failure(let error):
-                print("error here: \(error)")
-            }
+            this.handleResult(result)
+        }
+    }
+    
+    private func handleResult(_ result: Result<WeatherModel, Error>) {
+        switch result {
+        case .success(let model):
+            updateView(with: model)
+        case .failure(let error):
+            print("error here: \(error)")
         }
     }
     
@@ -120,19 +133,7 @@ extension WeatherViewController: CLLocationManagerDelegate {
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         if let location = locations.last {
             manager.stopUpdatingLocation()
-            let lat = location.coordinate.latitude
-            let lng = location.coordinate.longitude
-            
-            weatherManager.fetchWeather(lat: lat, lon: lng) { (result) in
-                switch result {
-                case .success(let model):
-                    DispatchQueue.main.async {
-                        self.updateView(with: model)
-                    }
-                case .failure(let error):
-                    print(error.localizedDescription)
-                }
-            }
+            fetchWeather(byLocation: location)
         }
     }
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
